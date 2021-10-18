dfc752a4621ae282d0611a4802d5a9cf7794e754
nikola4468 <nikola4468@gmail.com>
2021-10-18 14:17:10 +0900

    add test2

--- src/test/java/DebitCardTest.java
+++ src/test/java/DebitCardTest.java
@@ -18,4 +18,14 @@ public class DebitCardTest {
         $(".paragraph_theme_alfa-on-white").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
 
+    @Test
+    void shouldSubmitRequest2() {
+        open("http://localhost:9999");
+        SelenideElement form = $(".form_theme_alfa-on-white");
+        form.$("[data-test-id=name] input").setValue("Василий Иванов-Петров");
+        form.$("[data-test-id=phone] input").setValue("+79270000000");
+        form.$("[data-test-id=agreement]").click();
+        form.$(".button__content").click();
+        $(".paragraph_theme_alfa-on-white").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
 }
