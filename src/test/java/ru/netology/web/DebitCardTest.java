6fdc05d08621bd74d525967d8c80a7430a9dbea3
nikola4468 <nikola4468@gmail.com>
2021-10-18 23:27:33 +0900

    correction test2

--- src/test/java/ru/netology/web/DebitCardTest.java
+++ src/test/java/ru/netology/web/DebitCardTest.java
@@ -1,5 +1,7 @@
 package ru.netology.web;
+
 import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.exactText;
@@ -8,36 +10,67 @@ import static com.codeborne.selenide.Selenide.open;
 
 public class DebitCardTest {
 
+    @BeforeEach
+    void setUp() {
+        open("http://localhost:9999");
+    }
+
     @Test
     void shouldSubmitRequest() {
-        open("http://localhost:9999");
         SelenideElement form = $(".form_theme_alfa-on-white");
         form.$("[data-test-id=name] input").setValue("Василий Иванов-Петров");
         form.$("[data-test-id=phone] input").setValue("+79270000000");
         form.$("[data-test-id=agreement]").click();
         form.$(".button__content").click();
-        $(".paragraph_theme_alfa-on-white").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+
+    @Test
+    void shouldSubmitRequestNoData() {
+        SelenideElement form = $(".form_theme_alfa-on-white");
+        form.$(".button__content").click();
+        $(".input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
     }
 
     @Test
     void shouldSubmitRequestBadName() {
-        open("http://localhost:9999");
         SelenideElement form = $(".form_theme_alfa-on-white");
         form.$("[data-test-id=name] input").setValue("Vasiliy");
-        form.$("[data-test-id=phone] input").setValue("+79270000000");
-        form.$("[data-test-id=agreement]").click();
         form.$(".button__content").click();
-        $(".input_type_text .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+        $(".input_invalid .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
+
+    @Test
+    void shouldSubmitRequestBadTelNoName() {
+        SelenideElement form = $(".form_theme_alfa-on-white");
+        form.$("[data-test-id=phone] input").setValue("879270000000");
+        form.$(".button__content").click();
+        $(".input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
     }
 
     @Test
     void shouldSubmitRequestBadTel() {
-        open("http://localhost:9999");
         SelenideElement form = $(".form_theme_alfa-on-white");
         form.$("[data-test-id=name] input").setValue("Василий Иванов-Петров");
         form.$("[data-test-id=phone] input").setValue("879270000000");
-        form.$("[data-test-id=agreement]").click();
         form.$(".button__content").click();
-        $(".input_type_tel .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+        $(".input_invalid .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+    }
+
+    @Test
+    void shouldSubmitRequestNoTel() {
+        SelenideElement form = $(".form_theme_alfa-on-white");
+        form.$("[data-test-id=name] input").setValue("Василий Иванов-Петров");
+        form.$(".button__content").click();
+        $(".input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    void shouldSubmitRequestNoCheckbox() {
+        SelenideElement form = $(".form_theme_alfa-on-white");
+        form.$("[data-test-id=name] input").setValue("Василий Иванов-Петров");
+        form.$("[data-test-id=phone] input").setValue("+79270000000");
+        form.$(".button__content").click();
+        $(".input_invalid .checkbox__text").shouldHave(exactText("Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй"));
     }
 }
