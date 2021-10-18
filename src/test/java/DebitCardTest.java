0fca90dc528b94f155d1f1f5f2050f0581e60a7b
nikola4468 <nikola4468@gmail.com>
2021-10-18 14:02:03 +0900

    add test

---
+++ src/test/java/DebitCardTest.java
@@ -0,0 +1,21 @@
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class DebitCardTest {
+
+    @Test
+    void shouldSubmitRequest() {
+        open("http://localhost:9999");
+        SelenideElement form = $(".form_theme_alfa-on-white");
+        form.$("[data-test-id=name] input").setValue("Василий");
+        form.$("[data-test-id=phone] input").setValue("+79270000000");
+        form.$("[data-test-id=agreement]").click();
+        form.$(".button__content").click();
+        $(".paragraph_theme_alfa-on-white").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+
+}
