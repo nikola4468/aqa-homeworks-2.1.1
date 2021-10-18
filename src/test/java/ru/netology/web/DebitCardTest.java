6e8aa5f940d3150691c8870b53b08f0f0e305539
nikola4468 <nikola4468@gmail.com>
2021-10-18 21:58:05 +0900

    correction test

--- src/test/java/ru/netology/web/DebitCardTest.java
+++ src/test/java/ru/netology/web/DebitCardTest.java
@@ -12,7 +12,7 @@ public class DebitCardTest {
     void shouldSubmitRequest() {
         open("http://localhost:9999");
         SelenideElement form = $(".form_theme_alfa-on-white");
-        form.$("[data-test-id=name] input").setValue("Василий");
+        form.$("[data-test-id=name] input").setValue("Василий Иванов-Петров");
         form.$("[data-test-id=phone] input").setValue("+79270000000");
         form.$("[data-test-id=agreement]").click();
         form.$(".button__content").click();
@@ -20,13 +20,24 @@ public class DebitCardTest {
     }
 
     @Test
-    void shouldSubmitRequest2() {
+    void shouldSubmitRequestBadName() {
         open("http://localhost:9999");
         SelenideElement form = $(".form_theme_alfa-on-white");
-        form.$("[data-test-id=name] input").setValue("Василий Иванов-Петров");
+        form.$("[data-test-id=name] input").setValue("Vasiliy");
         form.$("[data-test-id=phone] input").setValue("+79270000000");
         form.$("[data-test-id=agreement]").click();
         form.$(".button__content").click();
-        $(".paragraph_theme_alfa-on-white").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+        $(".input_type_text .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
+
+    @Test
+    void shouldSubmitRequestBadTel() {
+        open("http://localhost:9999");
+        SelenideElement form = $(".form_theme_alfa-on-white");
+        form.$("[data-test-id=name] input").setValue("Василий Иванов-Петров");
+        form.$("[data-test-id=phone] input").setValue("879270000000");
+        form.$("[data-test-id=agreement]").click();
+        form.$(".button__content").click();
+        $(".input_type_tel .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
     }
 }
